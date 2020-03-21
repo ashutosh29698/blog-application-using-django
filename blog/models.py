f3f1427e00a9b603227f9a879d7ef2207916bdef
ashutosh29698 <dhondkarashutosh@gmail.com>
2020-03-21 18:33:31 +0530

    final

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length = 20)
+    title = models.CharField(max_length = 50)
     description = models.TextField()
     image = models.ImageField(blank=True, upload_to='pics')
     date_posted = models.DateTimeField(auto_now_add = True)
